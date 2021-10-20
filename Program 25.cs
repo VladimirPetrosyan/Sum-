6ef728bf288dd5ae1c973f727bc545ad5bec5452
VladimirPetrosyan <92800312+VladimirPetrosyan@users.noreply.github.com>
2021-10-20 13:39:21 +0300

    Add files via upload

---
+++ Program 25.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cycle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = Convert.ToInt32(Console.ReadLine());
+            int a = 0, count = 0;
+            for (int i = 1; i<=N; i++){
+                if (i % 2 == 0)
+                {
+                    a = a + i;
+                    count = count + 1;
+                         
+                }
+
+            }
+            Console.WriteLine(count);
+            Console.WriteLine(a);
+            Console.ReadKey();
+
+        }
+    }
+}
